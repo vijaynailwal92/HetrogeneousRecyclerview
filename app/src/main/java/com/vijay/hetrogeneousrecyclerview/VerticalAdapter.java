1eed89b735835a13d68760e6e19aa033cb6faaa7
vijaynailwal1992 <vijaynailwal92@gmail.com>
2019-05-26 19:51:41 +0530

    Initial commit

---
+++ app/src/main/java/com/vijay/hetrogeneousrecyclerview/VerticalAdapter.java
@@ -0,0 +1,54 @@
+package com.vijay.hetrogeneousrecyclerview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class VerticalAdapter extends RecyclerView.Adapter<VerticalAdapter.MyViewHolder> {
+
+    private ArrayList<SingleVertical> data = new ArrayList<>();
+
+    public VerticalAdapter(ArrayList<SingleVertical> data) {
+        this.data = data;
+    }
+
+    @Override
+    public VerticalAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.vertical_single_row, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(VerticalAdapter.MyViewHolder holder, int position) {
+        holder.image.setImageResource(data.get(position).getImage());
+        holder.title.setText(data.get(position).getHeader());
+        holder.description.setText(data.get(position).getSubHeader());
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView image;
+        TextView title, description;
+
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            image = itemView.findViewById(R.id.image);
+            title = itemView.findViewById(R.id.title);
+            description = itemView.findViewById(R.id.description);
+
+
+        }
+    }
+}
